4419b869f3ec2edd12108ac068855f97437f3b05
GreyPencil <kangsuze@gmail.com>
2021-09-26 19:36:24 -0400

    basic list

--- react_staging/src/App.js
+++ react_staging/src/App.js
@@ -5,12 +5,22 @@ import List from './components/List'
 import Footer from './components/Footer'
 
 export default class App extends Component {
+  state = {
+    todos:
+    [
+      {id: "001", name: "Eat", done: true},
+      {id: "002", name: "Sleep", done: true},
+      {id: "003", name: "Shopping", done: false},
+      {id: "004", name: "Toileting", done: false}
+    ]
+  }
   render() {
+    const {todos} = this.state
     return (
       <div className="todo-container">
         <div className="todo-wrap">
           <Header />
-          <List />
+          <List todos = {todos}/>
           <Footer />
         </div>
      </div>
