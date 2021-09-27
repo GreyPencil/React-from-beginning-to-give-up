80599915d65c90888722f700d01bd3656c92184c
GreyPencil <kangsuze@gmail.com>
2021-09-27 14:25:15 -0400

    set proxy

--- react_staging/src/App.jsx
+++ react_staging/src/App.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class App extends Component {
+
+    handleClick = () => {
+        axios.get('http://localhost:3000/api1/students').then(
+            response => {console.log('succeed', response.data)},
+            error => {console.log('failed', error)}
+        )
+        
+    }
+
+    handleClickCar = () => {
+        axios.get('http://localhost:3000/api2/cars').then(
+            response => {console.log('succeed', response.data)},
+            error => {console.log('failed', error)}
+        )
+        
+    }
+
     render() {
         return (
             <div>
-                App
+                <button onClick={this.handleClick}>Click me to get Student data</button>
+                <hr/>
+                <button onClick={this.handleClickCar}>Click me to get Car data</button>
             </div>
         )
     }
