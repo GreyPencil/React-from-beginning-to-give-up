2b32df38f837a7dd50c011bb8f330c82083370e9
GreyPencil <kangsuze@gmail.com>
2021-09-27 10:00:51 -0400

    footer

--- react_staging/src/components/Footer/index.jsx
+++ react_staging/src/components/Footer/index.jsx
@@ -3,15 +3,20 @@ import './index.css'
 
 export default class Footer extends Component {
     render() {
+        const {todos} = this.props;
+        const totalCount = todos.length;
+        const doneCount = todos.reduce((pre, todo) => 
+            pre + (todo.done ? 1:0) , 0
+        )
         return (
             <div className="todo-footer">
                 <label>
                     <input type="checkbox" />
                 </label>
                 <span>
-                    <span>Completed: 0</span> / All tasks: 2
+                    <span>Completed: {doneCount}</span> / All tasks: {totalCount}
                 </span>
-                <button className="btn btn-danger">Clear all compoleted tasks</button>
+                <button className="btn btn-danger" onClick={this.handleDelete}>Clear all compoleted tasks</button>
             </div>
         )
     }
