26d7b903c2c970cc009f4dce8a82df6a52f82260
GreyPencil <kangsuze@gmail.com>
2021-09-29 12:34:36 -0400

    prepare react-redux

--- redux_test/src/components/Count/index.jsx
+++ redux_test/src/components/Count/index.jsx
@@ -1,51 +1,26 @@
 import React, { Component } from 'react'
-import store from '../../redux/store'
-import { 
-    createIncrementAction, 
-    createDecrementAction,
-    createDecrementAsyncAction
-} from '../../redux/count_action_creators'
 
 export default class Count extends Component {
-    // state = { count: 0 }
-
-    // componentDidMount(){
-    //     store.subscribe(() => {
-    //         this.setState({})
-    //     })
-    // }
 
     increment = () => {
         const { value } = this.selectNumber
         // const { count } = this.state
         // this.setState({ count: count + value * 1 })
-        store.dispatch(createIncrementAction(value*1))
     }
 
     decrement = () => {
         const { value } = this.selectNumber
         // const { count } = this.state
         // this.setState({ count: count - value * 1 })
-        store.dispatch(createDecrementAction(value*1))
     }
 
     incrementIfOdd = () => {
 
         const { value } = this.selectNumber
-        const count  = store.getState()
-        if (count % 2 !== 0) {
-            // store.dispatch({type: 'increment', data: value*1})
-            store.dispatch(createIncrementAction(value*1))
-        }
     }
 
     incrementAsync = () => {
         const {value} = this.selectNumber
-        // const {count} = this.state
-        // setTimeout(() => {
-            // store.dispatch({type: 'increment', data: value*1})
-            store.dispatch(createDecrementAsyncAction(value*1,500))
-        // },1000)
         
     }
 
@@ -53,7 +28,7 @@ export default class Count extends Component {
         // const { count } = this.state
         return (
             <div>
-                <h1>Sum: {store.getState()}</h1>
+                <h1>Sum: </h1>
                 <select name="number" ref={c => this.selectNumber = c}>
                     <option value="1">1</option>
                     <option value="2">2</option>
