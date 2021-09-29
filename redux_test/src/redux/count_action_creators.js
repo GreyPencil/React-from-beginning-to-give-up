64b6d077acd195aa50ef522bd9e6476abf58120e
GreyPencil <kangsuze@gmail.com>
2021-09-28 22:43:11 -0400

    create two actions

---
+++ redux_test/src/redux/count_action_creators.js
@@ -0,0 +1,7 @@
+// function createIncrementAction(data){
+//     return {type:'increment', data:data}
+// }
+
+export const createIncrementAction = (data) => ({type:'increment', data })
+export const createDecrementAction = (data) => ({type:'decrement', data })
+    
