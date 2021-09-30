37f59b6f632f60799c4c0128036e94190982b45a
GreyPencil <kangsuze@gmail.com>
2021-09-30 15:24:06 -0400

    06 prepare to add person

--- redux_test/src/App.jsx
+++ redux_test/src/App.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
-import Count  from './containers/Count'
-import store from './redux/store'
-
+import Count from './containers/Count'
+import Person from './containers/Person'
 
 export default class App extends Component {
-    render() {
-        return (
-            <div>
-                <Count store={store}/>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div>
+				<Count/>
+				<hr/>
+				<Person/>
+			</div>
+		)
+	}
 }
