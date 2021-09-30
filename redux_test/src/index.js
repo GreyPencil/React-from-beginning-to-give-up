11892fe739ad269ebbf4f848c9296795c499b62a
GreyPencil <kangsuze@gmail.com>
2021-09-30 13:51:15 -0400

    updated provider

--- redux_test/src/index.js
+++ redux_test/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import store from './redux/store'
+import store from './redux/store'
+import {Provider} from 'react-redux'
 import App from './App';
 
 ReactDOM.render(
-    <App />,
+  <Provider store ={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
