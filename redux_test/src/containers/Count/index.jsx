b66037aced2fa039399d40ca5683f204bcdf1385
GreyPencil <kangsuze@gmail.com>
2021-09-30 13:39:30 -0400

    simple mapdispath

--- redux_test/src/containers/Count/index.jsx
+++ redux_test/src/containers/Count/index.jsx
@@ -3,18 +3,25 @@ import {connect} from 'react-redux'
 import { createIncrementAction, createDecrementAction, createDecrementAsyncAction } from '../../redux/count_action_creators'
 
 
-function mapStateToProps(state){
-    return {count:state}
-}
+// function mapStateToProps(state){
+//     return {count:state}
+// }
 
-function mapDispatchToProps(dispatch){
-    return {
-        add: number => dispatch (createIncrementAction(number)),
-        minus: number => dispatch(createDecrementAction(number)),
-        asyncAdd: (number,time) => dispatch(createDecrementAsyncAction(number,time))
-}
-}
+// function mapDispatchToProps(dispatch){
+//     return {
+//         add: number => dispatch (createIncrementAction(number)),
+//         minus: number => dispatch(createDecrementAction(number)),
+//         asyncAdd: (number,time) => dispatch(createDecrementAsyncAction(number,time))
+// }
+// }
 
 
-export default connect(mapStateToProps,mapDispatchToProps)(CountUI)
+export default connect(
+    state => ({count:state}),
+    {
+                add: createIncrementAction,
+                minus: createDecrementAction,
+                asyncAdd: createDecrementAsyncAction
+        }
+        )(CountUI)
 
