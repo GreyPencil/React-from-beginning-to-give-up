63945d6ad258bbf6c594ddd51a348a5a7c562e3c
GreyPencil <kangsuze@gmail.com>
2021-09-30 13:47:34 -0400

    updated provider

--- redux_test/src/index.js
+++ redux_test/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import store from './redux/store'
+// import store from './redux/store'
 import App from './App';
 
 ReactDOM.render(
@@ -8,8 +8,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-store.subscribe(() => {
-  ReactDOM.render(
-    <App />,
-  document.getElementById('root'));
-})
+// store.subscribe(() => {
+//   ReactDOM.render(
+//     <App />,
+//   document.getElementById('root'));
+// })
