7b448f3c731c62edc78d9d4dbe0420b37c423d2e
GreyPencil <kangsuze@gmail.com>
2021-09-30 14:10:34 -0400

    combined ui and containter

---
+++ redux_test/5_src/containers/Count/index.jsx
@@ -0,0 +1,27 @@
+import CountUI from '../../components/Count'
+import {connect} from 'react-redux'
+import { createIncrementAction, createDecrementAction, createDecrementAsyncAction } from '../../redux/count_action_creators'
+
+
+// function mapStateToProps(state){
+//     return {count:state}
+// }
+
+// function mapDispatchToProps(dispatch){
+//     return {
+//         add: number => dispatch (createIncrementAction(number)),
+//         minus: number => dispatch(createDecrementAction(number)),
+//         asyncAdd: (number,time) => dispatch(createDecrementAsyncAction(number,time))
+// }
+// }
+
+
+export default connect(
+    state => ({count:state}),
+    {
+                add: createIncrementAction,
+                minus: createDecrementAction,
+                asyncAdd: createDecrementAsyncAction
+        }
+        )(CountUI)
+
